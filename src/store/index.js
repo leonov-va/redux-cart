fe5d06d43c90e4ccdedf27f96e0d852ab79a981c
Vladislav Leonov <leonov.xi@gmail.com>
2021-09-23 23:28:30 +0300

    init middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,11 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import reducers from "./reducers";
 
-export default createStore(reducers);
+const logger = (store) => (next) => (action) => {
+  console.log("Предыдущее состояние", store.getState());
+  console.log("Экшен", action.type);
+  next(action);
+  console.log("Новое состояние", store.getState());
+};
+
+export default createStore(reducers, applyMiddleware(logger));
